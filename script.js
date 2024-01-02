f71ecff89523936416734d7a526f7b89069abbae
Ianto Lin Xi <iantolinxi@gmail.com>
2024-01-02 17:49:11 -0500

    make scrolling more robust

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const loadTime = new Date()
+
 const loadYaml = async path => {
   try {
     // Fetch the YAML file
@@ -34,47 +36,47 @@ const eventSpeaker = ({
   name,
 }) => `<a class="event-speaker" href="#speaker-${sid}">${name}</a>`
 
+const getHourTimestamp = (date=loadTime) => {
+  date = new Date(date)
+  // Round down to the nearest hour
+  date.setMinutes(0, 0, 0) // Sets minutes, seconds, and milliseconds to 0
+  const timestamp = `${date.getTime()}`
+  return timestamp
+}
+
+
 const event = allSpeakers => ({
   name,
   datetime,
   location,
   speakers,
-}) => `<div class="event" id="event-${datetime}">
-  <div class="name">${name}</div>
-  <div class="datetime">${datetime}</div>
-  <div class="location">${location}</div>
-  <div class="speakers">${speakers.map(s => eventSpeaker(allSpeakers[s])).join()}</div>
-</div>`
+}) => {
+  const date = new Date(datetime)
+  return `<div class="event" id="event-${getHourTimestamp(date)}">
+    <div class="name">${name}</div>
+    <div class="details">
+      <div class="location">${location}</div>
+      <div class="datetime">${new Intl.DateTimeFormat("en", {dateStyle: "medium", timeStyle: "short"}).format(date)}</div>
+    </div>
+    <div class="speakers">${speakers.map(s => eventSpeaker(allSpeakers[s])).join()}</div>
+  </div>`
+}
 
 const renderEvents = (speakers, events) => {
   const contentDiv = document.getElementById('events')
   contentDiv.innerHTML = events.map(event(speakers)).join("")
 }
 
-const getNowHour = () => {
-  const now = new Date()
-  // Round down to the nearest hour
-  now.setMinutes(0, 0, 0) // Sets minutes, seconds, and milliseconds to 0
-  // Format the timestamp
-  const year = now.getFullYear()
-  const month = String(now.getMonth() + 1).padStart(2, '0') // Months are 0-indexed
-  const day = String(now.getDate()).padStart(2, '0')
-  const hour = String(now.getHours()).padStart(2, '0')
-
-  const timestamp = `${year}-${month}-${day}-${hour}:00`
-  return timestamp
-}
-
 const scrollToHour = () => {
-  const timestamp = getNowHour()
+  const timestamp = getHourTimestamp()
   const eventDiv = document.getElementById(`event-${timestamp}`)
   if (eventDiv) {
     eventDiv.scrollIntoView({ behavior: 'smooth' })
   }
 }
 const renderTestScroll = () => {
-  const contentDiv = document.getElementById('test-event')
-  contentDiv.innerHTML = `<div id="event-${getNowHour()}"></div>`
+  const contentDiv = document.getElementById('scroll-test-event')
+  contentDiv.innerHTML = `<div id="event-${getHourTimestamp()}"></div>`
 }
 
 const render = async () => {
